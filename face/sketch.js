657b27d61498615ac3bf19bf64e83b43ce1bd149
hyunseo2810 <hyunseo2810@gmail.com>
2026-05-08 16:02:36 +0900

    Add files via upload

---
+++ face/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  
+  background(220);
+  fill(242, 216, 143);
+  ellipse(200, 200, 200, 220);
+  
+  fill(0, 0, 0)
+  ellipse(165, 165, 20, 20);
+  
+  ellipse(235, 165, 20, 20);
+  
+  fill(242,216,14);
+  triangle(180,230, 200, 180, 220, 230)
+  
+  arc(250, 250, 250)
+  
+
+}
